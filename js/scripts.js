6434098283af9762c3208e784875e14147fef32d
theMasudRana <mr.masudrana00@gmail.com>
2018-01-21 23:00:28 +0600

    Portfolio structured

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+(function ($) {
+	'use strict';
+
+	// Portfolio section 
+    $('.portfolio-section').imagesLoaded(function() {
+        // Portfolio activation
+        var $grid = $('.portfolio-grid').isotope({
+            itemSelector: '.portfolio-item',
+            percentPosition: true,
+        });
+
+        // Portfolio filtering activation
+        $('.portfolio-filter li a').on('click', function() {
+            var filterValue = $(this).attr('data-filter');
+            $grid.isotope({ filter: filterValue });
+        });
+
+        // filter menu class addition  
+        $('.portfolio-filter li').on('click', function(event) {
+            $(this).siblings('.active').removeClass('active');
+            $(this).addClass('active');
+            event.preventDefault();
+        });
+    });
+    
+})(jQuery)
